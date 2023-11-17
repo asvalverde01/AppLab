bda5079e2b316dbb756efb521cbe1f20cb120044
Alberto Sebastián Valverde González <sebastian.valverde@udla.edu.ec>
2023-11-17 12:15:25 -0600

    Backup

---
+++ AppLab/Models/TipoTrabajador.cs
@@ -0,0 +1,8 @@
+﻿namespace AppLab.Models
+{
+    public class TipoTrabajador
+    {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ AppLab/Services/TipoTrabajadorService.cs
@@ -0,0 +1,20 @@
+﻿using AppLab.Models;
+using System.Net.Http.Json;
+
+namespace AppLab.Services
+{
+    public class TipoTrabajadorService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TipoTrabajadorService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<TipoTrabajador>> GetTipoTrabajadoresAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<TipoTrabajador>>("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TipoTrabajador");
+        }
+    }
+}
