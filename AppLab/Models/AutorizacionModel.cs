53ff19af48b79d3311a1995437a746a75fa5deba
Alberto Sebastián Valverde González <sebastian.valverde@udla.edu.ec>
2023-11-17 18:02:12 -0600

    Auth agregado

---
+++ AppLab/Models/AutorizacionModel.cs
@@ -0,0 +1,15 @@
+﻿public class AutorizacionModel
+{
+    public string NOMBREUSUARIO { get; set; }
+    public string PERFIL { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string OBSERVACION { get; set; }
+    public string CODIGOPERFIL { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string ESTADO { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string COMPANIA { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string Emisor { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string Cargo { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string NOMBREEMISOR { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string NOMBRECOMPANIA { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string USUARIOCLIENTE { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+    public string RucUsuario { get; set; } // Aunque es null en la respuesta, se incluye por si acaso
+}
